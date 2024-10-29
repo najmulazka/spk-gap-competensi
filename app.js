e95ca168e8368f15afa9ea5373e788c2ef2b3c8c
Najmul Azka <135337785+najmulazka@users.noreply.github.com>
2024-10-29 09:10:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const { hitung } = require('./controllers/hitung.controllers');
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+//app.use((req, res, next) => {
+//  res.header('Access-Control-Allow-Origin', '*');
+//  next();
+//});
 
 // Router index
 app.get('/', (req, res) => {
